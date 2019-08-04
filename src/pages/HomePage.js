c3bcb97dc4e759cbe78a80394f62b7a81ff1eadd
Daniel Shokri <danielshokri@gmail.com>
2019-08-04 08:31:39 +0300

    Added hompage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-
-
+import UserService from '../services/UserService'
+import BitcoinService from '../services/BitcoinService'
 
 class HomePage extends Component {
+    state = { user: [], btc: null }
 
-    render(){
-        return(
+    async  componentDidMount() {
+        const user = UserService.getUser();
+        this.setState({ user });
+        const btc = await BitcoinService.getRate(user.coins)
+        this.setState({ btc });
+    }
+
+    render() {
+        const { user , btc} = this.state
+        console.log(user)
+        return (
             <div>
-                <h1>Welcome!!!</h1>
+                {user && (
+                    <section>
+                        <h1>Hello {user.name}!</h1>
+                        <div>Coins:{user.coins}</div>
+                        <div>BTC: {btc}</div>
+                    </section>
+                )}
             </div>
         )
     }
---
+++ src/services/UserService.js
@@ -0,0 +1,16 @@
+
+export default {
+    getUser
+}
+
+const users =
+{
+    name: 'Daniel',
+    coins: 100,
+    moves: []
+}
+
+
+function getUser() {
+    return users
+}
