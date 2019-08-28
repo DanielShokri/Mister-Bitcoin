31164ef91d2acfe438a74c73e62cc5d8827306e5
Daniel Shokri <danielshokri@gmail.com>
2019-08-28 13:30:26 +0300

    Updates

---
+++ precache-manifest.1f700e26f59cce81686c97e1e1262422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb791431d324f457849d908e2c30e649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f222043e998e0b7368f",
+    "url": "/static/css/main.cc174d9b.chunk.css"
+  },
+  {
+    "revision": "94f71282373a68223792",
+    "url": "/static/js/2.b613a487.chunk.js"
+  },
+  {
+    "revision": "4f222043e998e0b7368f",
+    "url": "/static/js/main.aa2d1607.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
